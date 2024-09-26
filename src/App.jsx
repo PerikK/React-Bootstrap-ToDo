67ba7a22e3bb9db6ae9848a0cc393e3be965c060
PerikK <perik76@gmail.com>
2024-09-26 11:26:17 +0300

    add absic functionality to ToDoApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Container } from "react-bootstrap"
+import ToDoApp from "./components/ToDoApp"
 
 function App() {
 
   return (
     <>
       <Container>
-      <h1 className="py-3">
-        ToDo App
-      </h1>
+        <h1 className="py-3">
+          ToDo App
+        </h1>
+        <ToDoApp/>
       </Container>
     </>
   )
--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,5 +1,44 @@
+import { useState } from 'react'
+import ToDoInput from './ToDoInput'
+import ToDoList from './ToDoList'
+
 export default function ToDoApp() {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = (input) => {
+        const newTodo = {
+            id: 1,
+            text: input,
+            completed: false
+        }
+        setTodos(prevTodos => [...prevTodos, newTodo])
+    }
+    
+    const deleteTodo = (id) => {
+        const filteredTodos = todos.filter(todo => todo.id !== id)
+        setTodos(filteredTodos)
+    }
+
+    const toggleComplete = (id) => {
+        const updatedTodos = todos.map(todo => (
+            todo.id === id ? {...todo, completed: !todo.completed}: todo
+        ))
+        setTodos(updatedTodos)
+    }
+    
+    const editTodo = (id, newInput) => {
+            const updatedTodos = todos.map(todo => (
+            todo.id === id ? {...todo, text: newInput}: todo
+        ))
+        setTodos(updatedTodos)}    
+    
+
+
+
     return (
-        <h2>Placeholder</h2>
+        <div>
+            <ToDoInput />
+            <ToDoList />
+        </div>
     )
 }
